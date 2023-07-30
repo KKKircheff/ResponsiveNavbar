45734a57b53b031c255adb6883a1fe80506e0e99
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-07-30 19:46:53 +0200

    mobile subscriptions ready

--- app/src/components/about-me-slider/about-me-slider.component.tsx
+++ app/src/components/about-me-slider/about-me-slider.component.tsx
@@ -1,7 +1,6 @@
 import './about-me-slider.styles.scss'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-
 import Slider from "react-slick";
 
 type Props = {
--- app/src/components/memberships-container/memberships-container.component.tsx
+++ app/src/components/memberships-container/memberships-container.component.tsx
@@ -1,7 +1,23 @@
 import { MembershipCard } from '../membership-card/membership-card.component'
 import './memberships-container.styles.scss'
 
+import { useEffect, useState } from 'react';
+
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 export const MembershipsContainer = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        // autoplaySpeed: 1000,
+    };
 
     const subscriptions = [
         {
@@ -29,15 +45,52 @@ export const MembershipsContainer = () => {
                 'Nutritional Guidance'],
         },
     ]
+
+    const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLargeScreen(window.innerWidth >= 920);
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Call handleResize to set initial value
+        handleResize();
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
     return (
         <div className='memberships-container'>
             <h1 className="memberships-container__title">Choose your membership plan</h1>
-            <div className="memberships-container__tiers">
-                {subscriptions.map((subscription, index) => {
-                    return <MembershipCard key={index} subscription={subscription} />
-                }
-                )}
-            </div>
+            {isLargeScreen
+                ? <div className="memberships-container__tiers">
+                    {subscriptions.map((subscription, index) => {
+                        return <MembershipCard key={index} subscription={subscription} />
+                    }
+                    )}
+                </div>
+                : <div className="memberships-container__mobile">
+                    <Slider {...settings}>
+                        {subscriptions.map((subscription, index) => {
+                            return (
+                                <div key={index}>
+                                    <MembershipCard key={index} subscription={subscription} />
+                                </div>
+                            )
+                        }
+                        )}
+                    </Slider>
+                </div>
+            }
+
+
+
+
         </div>
     )
 }
